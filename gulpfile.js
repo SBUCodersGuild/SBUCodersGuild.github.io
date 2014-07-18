a8d35c5b4fcc5f05febb1fbf5b27aa43b41d62db
Jill <Jaemu@users.noreply.github.com>
2014-07-17 21:17:09 -0400

    Update gulpfile.js
    Adding sass compiler to gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,27 @@
 var gulp = require('gulp');
 var changed = require('gulp-changed');
 var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
 
-var SRC = 'static/js/*.js';
-var DEST = 'static/build';
+var JS_SRC = 'static/js/*.js';
+var JS_DEST = 'static/build';
 
-gulp.task('default', function() {
-  return gulp.src(SRC)
-	.pipe(changed(DEST))
+var CSS_SRC = 'static/scss/*.scss';
+var CSS_DEST = 'static/build';
+
+
+gulp.task('js', function() {
+  return gulp.src(JS_SRC)
+	.pipe(changed(JS_DEST))
 	.pipe(uglify())
-	.pipe(gulp.dest(DEST));
+	.pipe(gulp.dest(JS_DEST));
 });
 
+gulp.task('sass', function(){
+	return gulp.src(CSS_SRC)
+		.pipe(sass)
+		.pipe(gulp.dest(CSS_DEST));
+})
+
+gulp.task('default', ['js', 'sass'])
 
