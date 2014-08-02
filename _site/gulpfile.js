a2faf7f6117a935db70b46f9b6a25b0273031494
Jill <jillian.munson@stonybrook.edu>
2014-08-02 17:23:48 -0400

    added github pages support with jekyll

---
+++ _site/gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var changed = require('gulp-changed');
+var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+
+var JS_SRC = 'static/js/*.js';
+var JS_DEST = 'static/build';
+
+var CSS_SRC = 'static/scss/*.scss';
+var CSS_DEST = 'static/build';
+
+
+gulp.task('js', function() {
+  return gulp.src(JS_SRC)
+	.pipe(changed(JS_DEST))
+	.pipe(uglify())
+	.pipe(gulp.dest(JS_DEST));
+});
+
+gulp.task('sass', function() {
+	return gulp.src(CSS_SRC)
+		.pipe(changed(CSS_DEST))
+		.pipe(sass({style: 'expanded', errLogToConsole: 'true'}))
+		.pipe(gulp.dest(CSS_DEST));
+});
+
+gulp.task('default', ['js', 'sass']);
+
